050003df535bc7857f212c2f65dc6ce17aaca0c9
g9y1b <g9y1b@ugrad.cs.ubc.ca>
2019-01-05 20:44:19 +0100

    reallly finalized plotting function

--- VOpipeline.m
+++ VOpipeline.m
@@ -354,14 +354,14 @@ end
 function plotall(image,Xhist,P,X,C,keepReprojection,keepAngle,keepBehind,...
     T, t_BA, sizes, anglex)
 
-    cam = T*[-anglex*10 0 anglex*10; 0 0 0; 10 0 10; 1 1 1];
+    cam = T*[-anglex*5 0 anglex*5; 0 0 0; 5 0 5; 1 1 1];
 
     % figure('name','11','units','normalized','outerposition',[0.1 0.1 0.85 0.8]);
     figure(11)
     set(gcf,'units','normalized','outerposition',[0.1 0.1 0.85 0.8]);
     % plot upper image with 3D correspondences
    
-    subplot(2,5,1:3)
+    subplot(5,8,[1:6,9:14])
     hold off;
     imshow(image);
     hold on;
@@ -376,25 +376,26 @@ function plotall(image,Xhist,P,X,C,keepReprojection,keepAngle,keepBehind,...
         'Angle too small',...
         'outside viewing angle',...
         'newly triangulated',...
-        'old 3D landmarks'},'FontSize',6)
+        'old 3D landmarks'},'FontSize',8)
     title('image with 3D correspondences and new kps')
     
     % bar diagram with sizes of different matrices
-    subplot(2,5,6)
+    subplot(5,8,[31 32 39 40])
     bar(sizes(:,1),sizes(:,2:end))
     legend({'#-landmarks','#-tracked keypoints','#-newly triangulated'},'FontSize',6)
     title('Array sizes over frames')
     
-    % plot lower middle as 2D point cloud
-    subplot(2,5,7)
+    % plot lower left as 2D point cloud
+    subplot(5,8,[17:19,25:27,33:35])
     plot(Xhist(:,1),Xhist(:,3),'bx',X(:,1),X(:,3),'rx','linewidth',1);
-    legend({'old landmarks','currently visible'},'FontSize',6)
+    legend({'old landmarks','currently visible'},'FontSize',8)
     title('2D Pointcloud')
     xlabel('x')
     ylabel('z')
+    axis equal
     
-    % plot lower right Camera Positions
-    subplot(2,5,8)
+    % plot lower middle Camera Positions
+    subplot(5,8,[20:22,28:30,36:38])
     cla;
     if size(t_BA,2)<21 
         plot([0 t_BA(1,:) T(1,4)],[0 t_BA(3,:) T(3,4)],'rx','linewidth',1)
@@ -403,24 +404,24 @@ function plotall(image,Xhist,P,X,C,keepReprojection,keepAngle,keepBehind,...
         plot([t_BA(1,end-19:end) T(1,4)],[t_BA(3,end-19:end) T(3,4)],'rx',...
             [0 t_BA(1,1:end-20)],[0 t_BA(3,1:end-20)],'kx',...
             T(1,4),T(3,4),'bo','linewidth',1)
-        legend({'cam BA','cam fixed'},'FontSize',6)
+        legend({'cam BA','cam fixed'},'FontSize',8)
     end
     title('Camera Position')
     xlabel('x')
     ylabel('z')
     axis equal
     
-    subplot(2,5,[4 5 9 10])
+    subplot(5,8,[7 8 15 16 23 24])
     cla;
     if size(t_BA)<21 
         plot(t_BA(1,:),t_BA(3,:),'.b','linewidth',3)
     else
-        plot(t_BA(1,:),t_BA(3,:),'.b','linewidth',3)
+        plot(t_BA(1,end-20:end),t_BA(3,end-20:end),'.b','linewidth',3)
     end
     hold on;
     plot(X(:,1),X(:,3),'rx','linewidth',1)
     plot(cam(1,:),cam(3,:),'g','linewidth',2)
-    legend({'cam pos','landmarks','cam viewing angle'},'FontSize',6)
+    legend({'cam pos','landmarks','cam viewing angle'},'FontSize',8)
     title('last 20 cam pos and current Landmarks')
     axis equal;
     
