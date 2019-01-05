d840d291f386c4c4c0d97d0b2eafee1590e5691e
PhiAbs <32536419+PhiAbs@users.noreply.github.com>
2019-01-05 22:40:42 +0100

    Cleanup. And added code such that all datasets can be started

--- VOpipeline.m
+++ VOpipeline.m
@@ -1,17 +1,44 @@
-%% initialize VO Pipeline with Kitty Dataset
+%% initialize VO Pipeline
 clear all; close all; clc;
 
+% choose your dataset
+ds = 0; % 0: KITTI, 1: Malaga, 2: parking
+
 %% define Path and load parameters
-path = 'datasets/kitti00/kitti';
-% need to set kitti_path to folder containing "00" and "poses"
-assert(exist('path', 'var') ~= 0);
-ground_truth = load([path '/poses/00.txt']);
-ground_truth = ground_truth(:, [end-8 end]);
-last_frame = 4540;
-% K = load([path '/00/K.txt']);
+if ds == 0
+    path = 'datasets/kitti00/kitti';
+    % need to set kitti_path to folder containing "00" and "poses"
+    assert(exist('path', 'var') ~= 0);
+    ground_truth = load([path '/poses/00.txt']);
+    ground_truth = ground_truth(:, [end-8 end]);
+    last_frame = 4540;
     K = [7.188560000000e+02 0 6.071928000000e+02
         0 7.188560000000e+02 1.852157000000e+02
         0 0 1];
+elseif ds == 1 
+    path = 'datasets/malaga-urban-dataset-extract-07';
+    % Path containing the many files of Malaga 7.
+    assert(exist('path', 'var') ~= 0);
+    images = dir([path ...
+        '/malaga-urban-dataset-extract-07_rectified_800x600_Images']);
+    left_images = images(3:2:end);
+    last_frame = length(left_images);
+    K = [621.18428 0 404.0076
+        0 621.18428 309.05989
+        0 0 1];
+elseif ds == 2
+    path = 'datasets/parking';
+    % Path containing images, depths and all...
+    assert(exist('path', 'var') ~= 0);
+    last_frame = 598;
+    K = load([path '/K.txt']);
+     
+    ground_truth = load([path '/poses.txt']);
+    ground_truth = ground_truth(:, [end-8 end]);
+else
+    assert(false);
+end
+
 cameraParams = cameraParameters('IntrinsicMatrix',K');
 
 % camera angles
@@ -22,6 +49,7 @@ angley = K(2,3)/K(2,2);
 addpath('code')
 
 % parameters
+last_bootstrap_frame = 2;
 bidirect_thresh = 3; % TODO  0.3: good
 maxDistance_essential = 0.1;  % 0.1 is too big for parking!! 0.01 might work as well
 maxNumTrials_Essential = 20000;
@@ -44,9 +72,17 @@ enable_plot = false;
 
 %% Bootstrapping
 
-img0 = uint8(single(imread([path '/00/image_0/' sprintf('%06d.png',0)])));
-img1 = uint8(single(imread([path '/00/image_0/' sprintf('%06d.png',1)])));
-img2 = uint8(single(imread([path '/00/image_0/' sprintf('%06d.png',2)])));
+% store first image
+if ds == 1
+    img0 = uint8(loadImage(ds, 1, path, left_images));
+    img1 = uint8(loadImage(ds, 2, path, left_images));
+    img2 = uint8(loadImage(ds, 3, path, left_images));
+else
+    img0 = uint8(loadImage(ds, 0, path, path));
+    img1 = uint8(loadImage(ds, 1, path, path));
+    img2 = uint8(loadImage(ds, 2, path, path));
+end
+
 
 points = detectHarrisFeatures(img0,'MinQuality',minQuality_Harris);
 points = nonmaxSuppression(points, nonmax_suppression_radius);
@@ -72,11 +108,10 @@ kpl = kpl(keep,:);
     maxNumTrials_Essential, 'MaxDistance', maxDistance_essential);
 kps = kps(keep,:);
 kpl = kpl(keep,:);
-% subplot(2,1,2)
-% showMatchedFeatures(img0,img2,kps,kpl)
 
 [R_WC,t_WC] = relativeCameraPose(E,cameraParams,kps,kpl);
-% transform to our system
+
+% transform to our convention
 R_WC = R_WC';
 t_WC = t_WC';
 T_WC = [R_WC,t_WC];
@@ -84,8 +119,6 @@ T_WC = [R_WC,t_WC];
 T_CW = [R_WC',-R_WC'*t_WC];
 
 %% triangulate Points
-% M = cameraMatrix(cameraParams,R_W_C', t_W_C'); % this would be the other
-% convention for triangulate...
 M = (K*T_CW)';
 [Xnew, reprojectionErrors] = triangulate( kps, kpl, (K*[eye(3) zeros(3,1)])', M);
 
@@ -103,8 +136,6 @@ keep = all((abs(Xnew_cam(1:2, :))<[anglex; angley].*Xnew_cam(3, :))'...
 
 S.X = Xnew(keep,:);
 S.P = kpl(keep,:);
-% S.C = kpl(~keep,:);
-% S.F = kps(~keep,:);
 S.C = kpl;
 S.F = kps;
 S.findP = find(keep);
@@ -121,11 +152,11 @@ kpl=kpl';
 S.C = [S.C; kpl];
 S.T = [S.T T_WC(:)*ones(1,size(kpl,1))];
 S.F = [S.F; kpl];
-S.Frames = [S.Frames, 2 * ones(1,size(S.C,1))]; % TODO hardcoded last bootstrap frame
+S.Frames = [S.Frames, last_bootstrap_frame * ones(1,size(S.C,1))]; 
 
 % store data for bundle adjustment
 cameraPoses_all = table;
-cameraPoses_all.ViewId(1) = uint32(2); % TODO: hardcoded number, is that the correct one? we start counting at 0
+cameraPoses_all.ViewId(1) = uint32(last_bootstrap_frame);
 cameraPoses_all.Orientation{1} = T_WC(1:3, 1:3);
 cameraPoses_all.Location{1} = T_WC(1:3, end)';
 S.P_BA(:, num_BA_frames, 1) = S.P(:,1);
@@ -165,17 +196,26 @@ cam_z = [];
 
 for i=3:last_frame
     
-    % get new image
+%     fprintf('\n\nProcessing frame %d\n=====================\n', i);
+    
     img_prev = img;
-    img = uint8(single(imread([path '/00/image_0/' sprintf('%06d.png',i)])));
+    
+    % get new image
+    if ds == 0
+        img = uint8(single(imread([path '/00/image_0/' ...
+            sprintf('%06d.png',i)])));
+    elseif ds == 1
+        img = uint8(single(rgb2gray(imread([path ...
+            '/malaga-urban-dataset-extract-07_rectified_800x600_Images/' ...
+            left_images(i+1).name]))));
+    elseif ds == 2
+        img = uint8(single(im2uint8(rgb2gray(imread([path ...
+            sprintf('/images/img_%05d.png',i)])))));
+    else
+        assert(false);
+    end
     
     % track features into new image
-%     [points, keepP] = trackP(img);
-%     S.P = points(keepP,:);
-%     S.X = S.X(keepP,:);
-%     [pointsP, keepP] = trackP(img);
-%     P = pointsP(keepP,:);
-%     X = S.X(keepP,:);
     [points, keepC] = trackC(img);
     S.keepX = find(keepC(S.findP));
     keepP = keepC(S.findP);
@@ -200,15 +240,14 @@ for i=3:last_frame
     S.P_BA(keep_P_BA, end+1, 1) = S.P(:,1);
     S.P_BA(keep_P_BA, end, 2) = S.P(:,2);
     keepCidx = find(keepC);
-%     S.findP = find(ismember(keepCidx,S.findP(ismember(S.findP,keepCidx))));
     S.findP = find(ismember(S.findP,keepCidx));
     
-    [R_WC, t_WC, keepP] = estimateWorldCameraPose(S.P,S.X,cameraParams,...
+    [R_WC, t_WC, ~] = estimateWorldCameraPose(S.P,S.X,cameraParams,...
         'MaxNumTrials', p3p_num_iter, 'MaxReprojectionError', p3p_pixel_thresh);
     R_WC = R_WC';
     t_WC = t_WC';
     
-        % we reproject the points on our own and define the threshold for the
+    % we reproject the points on our own and define the threshold for the
     % points that should get discarded
     R_C_W = R_WC';
     t_C_W = -R_C_W*t_WC;
@@ -260,20 +299,11 @@ for i=3:last_frame
                     & (Xnew_cam(3,:) > 0)' ...
                     & reprojectionErrors < reprojection_thresh ...
                     & triangulationAngles > triangAngleThres,2);
-    
-    % plot for debugging and tuning
-%     minQuality_Harris = 0.1;
-%     reprojection_thresh_p3p = 2;
-%     if size(S.P,1) < 20
-%         minQuality_Harris = 0.01;
-%         if size(S.P,1) < 10
-%             reprojection_thresh_p3p = 4;
-%         end
-%     end
 
     sizes=[sizes; i size(S.P,1) size(S.C(NonLms,:),1) nnz(keep)];    
     t_WC_BA = [t_WC_BA; cameraPoses_all.Location{end}];
     X_hist = [X_hist; S.X];
+    
     if size(sizes,1)>5
         sizes(1,:)=[];
     end
@@ -289,16 +319,11 @@ for i=3:last_frame
     Xnew = Xnew(keep,:);
     S.X = [S.X; Xnew];
     S.P = [S.P; S.C(NonLms(keep),:)];
-    % S.P = [S.P; S.C(keep,:)];
-%     S.C = S.C(~keep,:);
-%     S.T = S.T(:,~keep);
-%     S.F = S.F(~keep,:);
-%     S.Frames = S.Frames(~keep); 
+
     % update data for bootstrap
     keep_P_BA = [keep_P_BA; ones(size(Xnew, 1), 1)];
     S.P_BA(end+1:end+size(Xnew, 1), :, :) = S.C_trace_tracker(NonLms(keep), :, :);
     S.X_BA = [S.X_BA; Xnew];
-%     S.C_trace_tracker = S.C_trace_tracker(~keep, :, :);
     
     % extract new Keypints
     points = detectHarrisFeatures(img,'MinQuality', minQuality_Harris);
@@ -307,8 +332,7 @@ for i=3:last_frame
     
     % make sure that the same keypoint does not get tracked more than once
     kp_new_sorted_out = checkIfKeypointIsNew(kpl', ...
-          [(S.C)'], harris_rejection_radius);
-%         [(S.P)', (S.C)'], harris_rejection_radius);      
+          (S.C)', harris_rejection_radius);
     S.C = [S.C; kp_new_sorted_out'];
     S.C_trace_tracker(end+1:end+size(kp_new_sorted_out, 2), end, :) = kp_new_sorted_out';
     S.F = [S.F; kp_new_sorted_out'];
@@ -317,7 +341,7 @@ for i=3:last_frame
     setPoints(trackP, S.P);
     setPoints(trackC, S.C);
     
-            % add newest camera pose to all camera poses
+    % add newest camera pose to all camera poses
     cameraPoses_new = table;
     cameraPoses_new.ViewId(1) = uint32(i);
     cameraPoses_new.Orientation{1} = T_WC(1:3, 1:3);
@@ -345,113 +369,5 @@ for i=3:last_frame
     
     if enable_plot
         plotBundleAdjustment(cameraPoses_all);
-    end
-    
-    cam_x = [cam_x, T_WC(1,4)];
-    cam_z = [cam_z, T_WC(3,4)];
-    
-    i
-    
-end
-
-
-
-%% Functions
-
-function plotall(image,Xhist,P,X,C,keepReprojection,keepAngle,keepBehind,...
-    T, t_BA, sizes, anglex)
-
-    cam = T*[-anglex*5 0 anglex*5; 0 0 0; 5 0 5; 1 1 1];
-
-    % figure('name','11','units','normalized','outerposition',[0.1 0.1 0.85 0.8]);
-    figure(11)
-    set(gcf,'units','normalized','outerposition',[0.1 0.1 0.85 0.8]);
-    % plot upper image with 3D correspondences
-   
-    subplot(5,8,[1:6,9:14])
-    hold off;
-    imshow(image);
-    hold on;
-    plot([0;C(~keepReprojection,1)],[0;C(~keepReprojection,2)],'bs','linewidth',1.5)
-    plot([0;C(~keepAngle,1)],[0;C(~keepAngle,2)],'cd','linewidth',1.5)
-    plot([0;C(~keepBehind,1)],[0;C(~keepBehind,2)],'b+','linewidth',1.5)
-    plot([0;C(keepAngle&keepReprojection&keepBehind,1)],...
-        [0;C(keepAngle&keepReprojection&keepBehind,2)],...
-        'yo','linewidth',1.5)
-    plot(P(:,1),P(:,2),'r*','linewidth',1.5)
-    legend({'ReprojectionError is too big',...
-        'Angle too small',...
-        'outside viewing angle',...
-        'newly triangulated',...
-        'old 3D landmarks'},'FontSize',8)
-    title('image with 3D correspondences and new kps')
-    
-    % bar diagram with sizes of different matrices
-    subplot(5,8,[31 32 39 40])
-    bar(sizes(:,1),sizes(:,2:end))
-    legend({'#-landmarks','#-tracked keypoints','#-newly triangulated'},'FontSize',6)
-    title('Array sizes over frames')
-    
-    % plot lower left as 2D point cloud
-    subplot(5,8,[17:19,25:27,33:35])
-    plot(Xhist(:,1),Xhist(:,3),'bx',X(:,1),X(:,3),'rx',T(1,4),T(3,4),'g+',...
-        cam(1,:),cam(3,:),'g','linewidth',1);
-    legend({'old landmarks','currently visible'},'FontSize',8)
-    title('2D Pointcloud')
-    xlabel('x')
-    ylabel('z')
-    axis equal
-    
-    % plot lower middle Camera Positions
-    subplot(5,8,[20:22,28:30,36:38])
-    cla;
-    if size(t_BA,2)<21 
-        plot([0 t_BA(1,:) T(1,4)],[0 t_BA(3,:) T(3,4)],'rx','linewidth',1)
-        legend({'Camera BA'},'FontSize',5)
-    else
-        plot([t_BA(1,end-19:end) T(1,4)],[t_BA(3,end-19:end) T(3,4)],'rx',...
-            [0 t_BA(1,1:end-20)],[0 t_BA(3,1:end-20)],'kx',...
-            T(1,4),T(3,4),'bo','linewidth',1)
-        legend({'cam BA','cam fixed'},'FontSize',8)
-    end
-    title('Camera Position')
-    xlabel('x')
-    ylabel('z')
-    axis equal
-    
-    subplot(5,8,[7 8 15 16 23 24])
-    cla;
-    if size(t_BA)<21 
-        plot(t_BA(1,:),t_BA(3,:),'.b','linewidth',3)
-    else
-        plot(t_BA(1,end-20:end),t_BA(3,end-20:end),'.b','linewidth',3)
-    end
-    hold on;
-    plot(X(:,1),X(:,3),'rx','linewidth',1)
-    plot(cam(1,:),cam(3,:),'g','linewidth',2)
-    legend({'cam pos','landmarks','cam viewing angle'},'FontSize',8)
-    title('last 20 cam pos and current Landmarks')
-    axis equal;
-    
-end
-
-
-%%
-function kp_new = checkIfKeypointIsNew(kp_new, kp_tracked, threshold)
-% checkIfKeypointIsNew checks if a newly found keypoint (Harris) already
-% existed as a keypoint which was tracked into the image from an earlier stage.
-% Discard a point if it is within a certain radius around an existing
-% keypoint
-% input: 
-% kn_new: newly found keypoints in Image, size 2xN
-% kp_tracked: keypoints tracked from earlier image, size 2xM
-% threshold: If a new point is closer to an existing keypoint than this
-% threshold, the point gets discarded
-% output: 
-% kp_new: sorted out keypoints, size 2xK
-
-for i = 1:length(kp_tracked)
-    distance = sqrt(sum((kp_new - kp_tracked(:,i)).^2));
-    kp_new = kp_new(:,distance > threshold);
-end
+    end    
 end
